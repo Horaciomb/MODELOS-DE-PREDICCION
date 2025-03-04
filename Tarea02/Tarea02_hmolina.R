3f51821f19fcb0536ca49a50a221305e3c1451ab
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2025-03-04 18:55:14 -0400

    interpretando tarea 2

--- Tarea02/Tarea02_hmolina.R
+++ Tarea02/Tarea02_hmolina.R
@@ -1,3 +1,8 @@
+
+
+
+
+
 # Ejercicio 1: Regresión Lineal Simple --------------------------------------------------
 modelo1 <- lm(mpg ~ wt, data = mtcars)
 cat("EJERCICIO 1 - Regresión Simple (mpg ~ wt):\n")
@@ -8,6 +13,7 @@ cat("\nExplicación Ejercicio 1:
 - Interpretación: Por cada 1000 lbs de aumento en peso, el mpg disminuye aproximadamente", abs(round(coef(modelo1)[2],1)), "unidades.
 - R²:", round(summary(modelo1)$r.squared, 2), "indica que el peso explica el", round(summary(modelo1)$r.squared*100, 1), "% de la variabilidad en mpg.
 - p-valor: <2e-16 (significativo) confirma una relación fuerte entre peso y mpg.\n\n")
+
 
 # Ejercicio 2: Modelo Polinomial (Grado 2) ----------------------------------------------
 modelo2 <- lm(mpg ~ wt + I(wt^2), data = mtcars)
